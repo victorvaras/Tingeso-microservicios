2c5b5d59ca87eaf18e8ac54b7bb3a99a262b5ddf
victor <victor.varas@usach.cl>
2024-11-21 00:23:59 -0300

    Inicializar microservicios

---
+++ Backend-microservicios/3- geteway-service/geteway-service/src/test/java/geteway/geteway_service/GetewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package geteway.geteway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
