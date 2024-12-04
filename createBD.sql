775019b1fcd6fc5a87df87e64f9bd9fb9d42e999
victorvaras <victor.varas@usach.cl>
2024-12-04 12:44:43 -0300

    config-data y createBD

---
+++ createBD.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE microservicio_cliente;
+CREATE DATABASE microservicio_evaluacion-credito;
+CREATE DATABASE microservicio_seguimiento-solicitud;
+CREATE DATABASE microservicio-simulacion-credito;
+CREATE DATABASE microservicio-solicitud-credito;
+CREATE DATABASE microservicio-tipo-prestamo;
